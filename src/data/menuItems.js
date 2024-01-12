aa9db5ebba7f4a96cfdd9a40583d75d0c55b7565
Jimy Marletta <84329916+honocoder@users.noreply.github.com>
2024-01-12 17:11:45 +0100

    almost finish site

---
+++ src/data/menuItems.js
@@ -0,0 +1,50 @@
+export const menuItems = [
+  {
+    id: 1,
+    name: 'Bulgobi DeopBap',
+    price: '13.70€',
+    image: 'https://res.cloudinary.com/jimymltta/image/upload/v1705073726/Yogi%20Food/Bulgobi_DeopBap_lgicx8.jpg',
+  },
+  {
+    id: 2,
+    name: 'BibimBap',
+    price: '13.70€',
+    image: 'https://res.cloudinary.com/jimymltta/image/upload/v1705073726/Yogi%20Food/BibimBap_cvgb9k.jpg',
+  },
+  {
+    id: 3,
+    name: 'K-Ramyun',
+    price: '14.70€',
+    image: 'https://res.cloudinary.com/jimymltta/image/upload/v1705073717/Yogi%20Food/K-Ramyun_usjhxf.jpg',
+  },
+  {
+    id: 4,
+    name: 'K-Curry',
+    price: '12.70€',
+    image: 'https://res.cloudinary.com/jimymltta/image/upload/v1705073716/Yogi%20Food/K-Curry_fk6kyd.jpg',
+  },
+  {
+    id: 5,
+    name: 'Jjajang',
+    price: '12.70€',
+    image: 'https://res.cloudinary.com/jimymltta/image/upload/v1705073726/Yogi%20Food/Jjajang_ud9rov.jpg',
+  },
+  {
+    id: 6,
+    name: 'Ddeokbokki',
+    price: '10.40€',
+    image: 'https://res.cloudinary.com/jimymltta/image/upload/v1705073726/Yogi%20Food/Ddeokbokki_porx6d.jpg',
+  },
+  {
+    id: 7,
+    name: 'KimBap',
+    price: '13€',
+    image: 'https://res.cloudinary.com/jimymltta/image/upload/v1705073716/Yogi%20Food/KimBap_vqip7m.jpg',
+  },
+  {
+    id: 8,
+    name: 'K-Chicken (uniquement le mercredi et le samedi)',
+    price: '6pcs : 11,70€ | 10pcs : 15€',
+    image: 'https://res.cloudinary.com/jimymltta/image/upload/v1705073727/Yogi%20Food/K-Chicken_m2gall.jpg',
+  },
+]
