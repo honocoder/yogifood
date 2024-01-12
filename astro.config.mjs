da483e20d8b5277355bb94196835983ffc79aa39
Jimy Marletta <84329916+honocoder@users.noreply.github.com>
2024-01-12 17:25:38 +0100

    trying again

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,12 @@
 import { defineConfig } from 'astro/config'
 import mdx from '@astrojs/mdx'
 import tailwind from '@astrojs/tailwind'
-import compress from 'astro-compress'
+import Compress from 'astro-compress'
 
 // https://astro.build/config
 export default defineConfig({
   compressHTML: true,
   integrations: [mdx(), tailwind({
     applyBaseStyles: false,
-  }), compress()],
+  }), Compress()],
 })
