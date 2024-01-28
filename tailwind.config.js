3cd62809de7c5d871c3d569d61889e5389749467
Jimy Marletta <84329916+honocoder@users.noreply.github.com>
2024-01-28 15:30:03 +0100

    change colors and make responsive better on homepage

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,11 @@
 module.exports = {
   content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'yogi-blue': '#22395C'
+      }
+    },
   },
   plugins: [],
   // darkMode: ['class', '.darkmode'], // uncomment this to enbable dark mode by default
